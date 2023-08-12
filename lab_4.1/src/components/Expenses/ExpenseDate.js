a729efda06bc2558dba7e96b25fbbd5d79c247af
sonht17102dev <121702671+sonht17102dev@users.noreply.github.com>
2023-08-12 10:52:20 +0700

    lab 4.1

---
+++ lab_4.1/src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,25 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sonht17102dev/Lab-React.git
+git push -u origin main
+*/
